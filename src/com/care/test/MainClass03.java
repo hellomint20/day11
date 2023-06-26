bb8565196ac1850c54e1e3b8cacec87282e134ea
seohee <test@test.123>
2023-06-26 16:48:50 +0900

    Package_동일한 클래스 이름

---
+++ src/com/care/test/MainClass03.java
@@ -0,0 +1,11 @@
+package com.care.test;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		com.java.sql.Date date = new com.java.sql.Date();
+		date.myDate();
+		
+		com.java.util.Date d = new com.java.util.Date();
+		d.myDate();
+	}
+}
