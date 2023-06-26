365a4f62eab1000f904d44b062475f1d0de2b161
seohee <test@test.123>
2023-06-26 16:45:38 +0900

    시간 관련

---
+++ src/clock/MainClass01.java
@@ -0,0 +1,17 @@
+package clock;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		long time = System.currentTimeMillis();
+		System.out.println(time);
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(end - time);
+		System.out.println((end-time)/1000 + "초 사용");
+	}
+}
