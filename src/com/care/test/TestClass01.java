68eb0b444f8c0909a910220fd3ee40a844c582ab
seohee <test@test.123>
2023-06-26 16:47:02 +0900

    Package 기본

---
+++ src/com/care/test/TestClass01.java
@@ -0,0 +1,12 @@
+package com.care.test;
+
+class AAA{}
+
+public class TestClass01 {
+	public void test() {
+		System.out.println("test");
+	}
+	void test2() {
+		System.out.println("test22");
+	}
+}
